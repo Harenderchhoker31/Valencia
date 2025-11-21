a42f9d9847a62a7d40c79d7105c5a9e58e5aafe0
Harenderchhoker31 <harenderchhoker31@gmail.com>
2025-11-22 01:53:07 +0530

    images of products download

---
+++ Valencia/src/assets/assets.js
@@ -0,0 +1,122 @@
+
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.png'
+import cart_icon from './cart_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+import product1 from './product1.png'
+import product2 from './product2.png'
+import product3 from './product3.png'
+import product4 from './product4.png'
+import product5 from './product5.png'
+import product6 from './product6.png'
+import product7 from './product7.png'
+import product8 from './product8.png'
+import product9 from './product9.png'
+import product10 from './product10.png'
+import product11 from './product11.png'
+import product12 from './product12.png'
+import product13 from './product13.png'
+import product14 from './product14.png'
+import product15 from './product15.png'
+import product16 from './product16.png'
+import product17 from './product17.png'
+import product18 from './product18.png'
+import product19 from './product19.png'
+import product20 from './product20.png'
+import product21 from './product21.png'
+import product22 from './product22.png'
+import product23 from './product23.png'
+import product24 from './product24.png'
+import product25 from './product25.png'
+import product26 from './product26.png'
+import product27 from './product27.png'
+import product28 from './product28.png'
+import product29 from './product29.png'
+import product30 from './product30.png'
+import product31 from './product31.png'
+import product32 from './product32.png'
+import product33 from './product33.png'
+import product34 from './product34.png'
+import product35 from './product35.png'
+import product36 from './product36.png'
+import product37 from './product37.png'
+import product38 from './product38.png'
+import product39 from './product39.png'
+import product40 from './product40.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon,
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    product7,
+    product8,
+    product9,
+    product10,
+    product11,
+    product12,
+    product13,
+    product14,
+    product15,
+    product16,
+    product17,
+    product18,
+    product19,
+    product20,
+    product21,
+    product22,
+    product23,
+    product24,
+    product25,
+    product26,
+    product27,
+    product28,
+    product29,
+    product30,
+    product31,
+    product32,
+    product33,
+    product34,
+    product35,
+    product36,
+    product37,
+    product38,
+    product39,
+    product40,
+
+
+}
